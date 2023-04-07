b00310a5e84853056ac3b15ff7006b106ef9bb0c
Mariia Syniushkina <marysinyushkina@gmail.com>
2023-04-07 19:54:08 +0200

    rmoived unused el

--- src/pages/BlogCardDetails.tsx
+++ src/pages/BlogCardDetails.tsx
@@ -1,5 +1,3 @@
-import MainImage from 'components/MainImage/MainImage'
-import background from 'assets/main-image/favourites-image.jpeg'
 import { BlogCard } from 'utils/blogCardsArray'
 import BlogCardExtended from 'components/BlogCard/BlogCardExtended'
 
@@ -9,7 +7,6 @@ type Props = {
 const BlogCardDetails = ({ data }: Props) => {
     return (
         <>
-            {/* <MainImage title="Article" img={background} /> */}
             <BlogCardExtended data={data} />
         </>
     )
